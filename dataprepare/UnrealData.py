60bc48a21f1c18b6b8010b1d60c5f6471635eb88
Goodtasterr <1261336144@qq.com>
2022-01-07 22:44:05 +0800

    增加  拟合轨道

--- dataprepare/UnrealData.py
+++ dataprepare/UnrealData.py
@@ -5,6 +5,10 @@ import os
 import math
 import copy
 sys.path.append('..')
+import time
+
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 class RailwayPointCloud():
     def __init__(self,  file_path):
@@ -18,6 +22,7 @@ class RailwayPointCloud():
         file.close()
         self.vec = np.asarray(data_list, dtype=np.float32)
         self.vec[:,(1,2)] *= -1
+        print("point cloud shape: ",self.vec.shape)
         self.pcd = o3d.geometry.PointCloud()
         self.pcd.points = o3d.utility.Vector3dVector(self.vec)
 
@@ -67,6 +72,7 @@ class RailwayPointCloud():
         plane_model, inliers = self.pcd.segment_plane(distance_threshold=0.1,
                                                  ransac_n=10,
                                                  num_iterations=1000)
+        print("inliers : ",(inliers[:10]))
         [a, b, c, d] = plane_model
         print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
 
@@ -108,131 +114,122 @@ class RailwayPointCloud():
 
         return points_ranged
 
-def ckeck_npyfile():
-    ##check npyfile
-    file_root = "../dataunreal/npyfile1/"
-    npy_paths = os.listdir(file_root)
-    for i, npy_path in enumerate(npy_paths):
-        npy_vec = np.load(os.path.join(file_root,npy_path))
-        print("npy_vec.shape: ", npy_vec.shape)
-
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(npy_vec)
-        mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
-        mesh.scale(3, center=mesh.get_center())
-        o3d.visualization.draw_geometries([pcd,mesh])
-
-def set_rotate_bias():
-    file_root = "../dataunreal/rawpointcloud1/" #前后，左右，上下
-    pc_names = os.listdir(file_root)
-    rotate_paras = np.asarray([-0.3, -0.3, -0.35, -0.37, -0.42, -0.42,
-                                -1.4, -1.4, -1.4, -1.4, -1.4, -1.36,
-                              0, 0, 0, 0, 0, 0])
-
-    rotate_paras2 = np.asarray([0, 0, 0, 0, 0,
-                              -1.36,-1.36,-1.36,-1.36,-1.312,
-                              -0.002, -0.002])
-    print(rotate_paras)
-    for i , pc_name in enumerate(pc_names) :
-        print(i+1)
-        railwayPC = RailwayPointCloud(os.path.join(file_root,pc_name))
-        railwayPC.pc_show(name=pc_name,rotate_para=rotate_paras[i])
-
-def label_points(points,parameter,range):
-    label = np.zeros([points.shape[0]]).astype(int)
-    ranged = copy.deepcopy(range)
-    if parameter is not None:
-        ranged[3] = ranged[3] + parameter[1]*(points[:,0]**2) + (parameter[0])*points[:,0]  # c + b*x + a*x^2
-        ranged[2]  += ranged[3] - 2
-
-    idx_x = (points[:,0] > range[0]) & (points[:,0] < range[1])
-    idx_y = (points[:,1] > ranged[2]) & (points[:,1] < ranged[3])
-    idx_z = (points[:,2] > range[4]) & (points[:,2] < range[5])
-    idx = idx_x & idx_y & idx_z
-    index = np.asarray(idx).astype(int)
-    label = index + label
-    return label,idx
-
-def pc_colors(arr):
+def label2color(labels):
+    """
+    将label 转为 open3d colors 的矩阵
+    :param labels: shape=[n,]
+    :return: clors: shape=[n,3] 值的范围[0,1]
+    """
     list = np.asarray([
+        [127,127,127],
         [255,0,0],
-        [0,255,0],
         [0,0,255],
         [127, 0, 127],
         [127, 127, 0]
-    ])
-    colors = []
-    for i in arr:
-        colors.append(list[i])
-    return np.asarray(colors)/255
+    ],dtype=np.float)
+    return list[labels]/255
 
-def lable_unrealdata():
-    '''
-    思路：
-        1.直轨道：使用矩形框；
-        2.左转/右转轨道：先预设二次函数[b,a]，精确调整[上，下]，找到一根铁轨表面部分点云（曲线形状），
-                        对这些点二次拟合获得准确的[b,a]并更新。
-    return:
-    '''
-    #lable params
-    rail_ranges =[6, 100, .1, 0.87,  -2.74, -2.2] # 前后，左右，上下  #[-2.5,-2.73] 轨道位置
-    parameter_lr = np.asarray([ 0.00114602, -0.00112116]).astype(np.float32)    # ax^2 + bx
+class RailProcess():
+    def __init__(self,  points, lables):
+        self.points = points
+        self.lables = lables
+        self.rail_points = points[lables == 1]
 
-    #lable 过程
-    file_root = "../dataunreal/npyfile2/"
+    def rail_points_count(self):
+        """
+        预设固定步进长度，分段计算轨道点的数量，
+        :return:
+        """
+        step = 5
+        rail_start_dist = int(np.min(self.rail_points[:,0]))
+        rail_end_dist = int(np.max(self.rail_points[:,0])) + 1
+        # n = int((rail_end_dist - rail_start_dist)/step) + 1
+        n=18
+        print(rail_start_dist,rail_end_dist,n)
+
+        count_arr = np.ones([n,2],dtype=np.int) #记录(起始距离，一段轨道点的数量)
+        for i in range(n):
+            start_dist = rail_start_dist + i * step
+            stop_dist = start_dist + step
+            idx = (self.rail_points[:,0] > start_dist) & (self.rail_points[:,0] < stop_dist)
+            rail_points_part = self.rail_points[idx]
+            #shape[0] 是点的数量
+            points_number = rail_points_part.shape[0]
+            count_arr[i][0] = start_dist
+            count_arr[i][1] = points_number
+
+        return count_arr
+
+    def fit_rail(self):
+        """
+        拟合轨道曲线
+        :return: [N,5] d1,d2,a,b,c
+        """
+        #1. 给定轨道点云
+        railpoints = self.rail_points
+        #2. 设定步长矩阵
+        d1_start = np.min(railpoints[:,0]) #起始距离区最小x的值
+        d_steps = [5,15,30,50] #步进的长度，共100米，根据情况改
+        curve_res = []
+        overlap_rate = 0.3
+        overlap_d = 0  #首次交叉区域为0
+        for step in d_steps:
+            d2_stop = d1_start + step #中止距离
+            part_idx = (railpoints[:, 0] > (d1_start-overlap_d)) and (railpoints[:, 0] < d2_stop)
+            part_points = railpoints[part_idx]
+
+            x_poly = part_points[:, 0:1] #[N,1] x
+            y_poly = part_points[:, 1:2] #[N,1] y
+            poly_features = PolynomialFeatures(degree=1, include_bias=False)
+            lin_reg = LinearRegression()
+            X_poly = poly_features.fit_transform(x_poly)
+            lin_reg.fit(X_poly, y_poly)
+            poly_para = [lin_reg.intercept_, lin_reg.coef_]
+            part_curve = [d1_start, d2_stop, lin_reg.coef_[0][0],poly_para[0],0]
+            curve_res.append(part_curve)  #得到结果，添加到res
+            overlap_d = step * overlap_rate  # 交叉率 * 步进距离 = 交叉距离
+
+        #得到了每次的一次函数拟合结果
+        #————————需要与上一次作对比，符合流程图，斜率差距大的，判定为弯轨道，进行二次函数拟合
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("非规则路面分割方法")
+    #1.load np array; 2.trans x-y-z to b-r-z [b=y/x, r=(x2+y2)^0.5]; 3.分配pixel坐标,二维的
+    file_root = "../dataunreal/npyfile1/"
     npy_paths = os.listdir(file_root)
-    show_flag = True
-    save_flag = True
+    show_flag = False
+    noise_flag = False
+    res_list = []
     for i, npy_path in enumerate(npy_paths):
-        if i>8 and i<10:
+        if i>=0 and i<20:
             file_path = os.path.join(file_root,npy_path)
             print(file_path)
             npy_vec = np.load(file_path)
-            noise = (np.random.rand(npy_vec.shape[0],npy_vec.shape[1])-0.5) * 0.03  # 正态分布[0~1]-0.5 再放大n倍
-            print("npy_vec.shape: ", npy_vec.shape)
-            # npy_vec = npy_vec + noise
-            label,idx = label_points(npy_vec,parameter_lr,rail_ranges)
+            rail_idx = npy_vec[:,-1]==1
 
-            new_points = np.concatenate((npy_vec,label[:,np.newaxis]),axis=1)
-            print(new_points.shape)
+            railProcess = RailProcess(npy_vec[:,:3],npy_vec[:,-1])
 
-            pcd = o3d.geometry.PointCloud()
-            pcd.points = o3d.utility.Vector3dVector(new_points[:,:3])
-            pcd.colors = o3d.utility.Vector3dVector(pc_colors(new_points[:,-1].astype(np.int)))
-            mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
-            mesh.scale(3, center=mesh.get_center())
-
-            from sklearn.preprocessing import PolynomialFeatures
-            from sklearn.linear_model import LinearRegression
-
-            rail_points = new_points[idx]
-            x_poly = rail_points[:, 0]
-            x_poly = x_poly[:, np.newaxis]
-            y_poly = rail_points[:, 1]
-            y_poly = y_poly[:, np.newaxis]
-
-            # degree=2 y =         c*x^2 + b*x + a
-            # degree=3 y = d*x^3 + c*x^2 + b*x + a
-            poly_features = PolynomialFeatures(degree=2, include_bias=False)
-            X_poly = poly_features.fit_transform(x_poly)
-            lin_reg = LinearRegression()
-            lin_reg.fit(X_poly, y_poly)
-            poly_para = [lin_reg.intercept_, lin_reg.coef_]
-            print('poly_para : ', poly_para)
+            if noise_flag:
+                noise_x = (np.random.rand(npy_vec.shape[0],1)-0.5) * 0.05  # 正态分布[0~1]-0.5 再放大n倍
+                noise_y = (np.random.rand(npy_vec.shape[0],1)-0.5) * 0.02  # 正态分布[0~1]-0.5 再放大n倍
+                noise_z = (np.random.rand(npy_vec.shape[0],1)-0.5) * 0.03  # 正态分布[0~1]-0.5 再放大n倍
+                noise = np.concatenate((noise_x,noise_y,noise_z),axis=1)
+                npy_vec[:,:3] += noise
 
             if show_flag:
-                o3d.visualization.draw_geometries([pcd,mesh],window_name=npy_path)
-            if save_flag:
-                short_name = npy_path.split(".")[0]
-                np.save(os.path.join(file_root, short_name), new_points)
-if __name__ == '__main__':
-    print()
+                pcd = o3d.geometry.PointCloud()
+                pcd.points = o3d.utility.Vector3dVector(npy_vec[:, :3])
+                pcd.colors = o3d.utility.Vector3dVector(label2color(npy_vec[:, -1].astype(np.int)))
+                mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+                mesh.scale(2, center=mesh.get_center())
+                # o3d.visualization.draw_geometries([pcd,mesh],window_name=npy_path)
 
-
-
-    # railwayPC.lable_rail()
-    # railwayPC.segment_ground()
-    # 1. 把xyz调好， 2.计算角度偏置并矫正角度， 3.轨道打标签
 
 
 
