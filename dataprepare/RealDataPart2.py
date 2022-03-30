eb37433e1eeffcf985926cde6653d9d2802b05c6
Goodtasterr <1261336144@qq.com>
2022-03-30 09:23:08 +0800

    real data -- finish part2

--- dataprepare/RealDataPart2.py
+++ dataprepare/RealDataPart2.py
@@ -16,7 +16,7 @@ def label2color(labels):
     list = np.asarray([
         [127,127,127],[0,0,200],[127,127,127],
         # [127,127,127],
-        [0, 0, 255],[127,127,127],[0, 0, 255],
+        [255, 0, 0],[127,127,127],[0, 0, 255],
         [0, 255, 0],
         [255,0,0],
         [0,0,255],
@@ -95,7 +95,7 @@ def poly_fit_iou(points,labels):
     poly_para = [lin_reg.intercept_, lin_reg.coef_]
     # print("poly params: ",poly_para)
 
-    y_bias = 0.93
+    y_bias = 0.95
     z_bias = 0.17
 
     y = poly_para[0] + poly_para[1][0][0] * points[:,0] + poly_para[1][0][1] * (points[:,0] ** 2)  # + poly_para[1][0][2]*x**3
@@ -128,14 +128,14 @@ def poly_fit_iou_ours(points,labels):
     step_x = 5.0 # 步进 x 米
     step_bais = 6.0 # 从 bais 米开始
     # rail_part_d = [4,9,13,17,21]
-    rail_cross = 2
+    rail_cross = 3
     delta_k = 0.1
     rail_part_d = [6,10,19,32,49,70]
     rail_part_range = [[rail_part_d[i],rail_part_d[i+1]] for i in range(5)]
 
     fit_para = []
     for i, part_range in enumerate(rail_part_range):
-        part_idx = (rail_points[:,0]>=part_range[0]) & (rail_points[:,0]<part_range[1])
+        part_idx = (rail_points[:,0]>=(part_range[0]-rail_cross)) & (rail_points[:,0]<part_range[1])
         rail_points_part = rail_points[part_idx]
 
         if len(rail_points_part) < 3:
@@ -150,7 +150,7 @@ def poly_fit_iou_ours(points,labels):
 
         if i > 0: #从1开始和上一次比较斜率b
             if (np.abs(fit_para[i][3] - fit_para[i-1][3]) > delta_k) \
-                    or (np.abs(fit_para[i][4] - fit_para[i-1][4]) > 0.1): #如果超过，则对后续的点二次拟合
+                    or (np.abs(fit_para[i][4] - fit_para[i-1][4]) > 0.3): #如果超过，则对后续的点二次拟合
                 poly_features2 = PolynomialFeatures(degree=2, include_bias=False)
                 part_idx2 = (rail_points[:, 0] >= fit_para[i-1][0]) & (rail_points[:, 0] < 70)
                 rail_points_part = rail_points[part_idx2]
@@ -162,68 +162,127 @@ def poly_fit_iou_ours(points,labels):
                 lin_reg.fit(X_poly, y_poly)
                 fit_para.pop() #删除当前段参数，因为它与上一段差距大。将上一段用2次函数代替
                 fit_para[i-1] = [fit_para[i-1][0],70,lin_reg.coef_[0][1],lin_reg.coef_[0][0],lin_reg.intercept_[0]]
-
                 break
     print("fit_para: ",fit_para)
-    #计算iou：
+
+    cal_iou = False
+    if cal_iou:
+        #计算iou：
+        for i, para in enumerate(fit_para):
+            idx_part = (points[:,0] >= para[0]) & (points[:,0] < para[1])  #取idx
+
+            points_part = points[idx_part]
+            labels_part = labels[idx_part]
+
+            y_bias = 0.85
+            y = para[4] + para[3] * points_part[:, 0] +para[2] * (
+                        points_part[:, 0] ** 2)
+            y_left = y - y_bias
+            y_right = y + y_bias
+
+            h_max = np.max(rail_points[:, 2]) + 0.1
+            h_min = np.min(rail_points[:, 2]) - 0.1
+
+            idx_iou = (points_part[:, 1] > y_left) & (points_part[:, 1] < y_right) \
+                  & (points_part[:, 0] > 6) & (points_part[:, 0] < 70) & (points_part[:, 2] > h_min) & (points_part[:, 2] < h_max)
+
+            pred_labels = labels_part[idx_iou]
+            pred_labels_rev = labels_part[~idx_iou]
+            # print("rail, bkg:",pred_labels.shape,pred_labels_rev.shape)
+            # print(pred_labels[pred_labels == 1].shape, pred_labels_rev[pred_labels_rev == 0].shape)
+            # exit()
+            iou_res = len(pred_labels[pred_labels == 1]) / (len(pred_labels[pred_labels == 0]) +
+                                                            len(pred_labels_rev[pred_labels_rev == 1]) + len(
+                        pred_labels[pred_labels == 1]))
+            print("iou: ", iou_res)
+            return iou_res
+
+    #路面分割
+    pcd_inner = o3d.geometry.PointCloud()
+    surface_points = []
+    noise_points = []
     for i, para in enumerate(fit_para):
-        idx_part = (points[:,0] >= para[0]) & (points[:,0] < para[1])  #取idx
-
+        print("start surface plane segmentation",i)
+        #构建安全区域
+        safe_y = 4.5
+        idx_part = (points[:, 0] >= para[0]) & (points[:, 0] < para[1])  # 取idx
         points_part = points[idx_part]
-        labels_part = labels[idx_part]
 
-        y_bias = 0.95
-        y = para[4] + para[3] * points_part[:, 0] +para[2] * (
-                    points_part[:, 0] ** 2)
-        y_left = y - y_bias
-        y_right = y + y_bias
+        y = para[4] + para[3] * points_part[:, 0] + para[2] * (points_part[:, 0] ** 2)
 
-        h_max = np.max(rail_points[:, 2]) + 0.1
-        h_min = np.min(rail_points[:, 2]) - 0.1
+        for j in range(18): #对idx_part 在y轴分段25段
+            y_start = y - safe_y/2 +0.25*j
 
-        idx_iou = (points_part[:, 1] > y_left) & (points_part[:, 1] < y_right) \
-              & (points_part[:, 0] > 6) & (points_part[:, 0] < 70) & (points_part[:, 2] > h_min) & (points_part[:, 2] < h_max)
+            idx_part_sub = (points_part[:, 1] > y_start) & (points_part[:, 1] < y_start+0.25) \
+                       & (points_part[:, 0] > 6) & (points_part[:, 0] < 70) \
+                       & (points_part[:, 2] > -4) & (points_part[:, 2] < 2)
 
-        pred_labels = labels_part[idx_iou]
-        pred_labels_rev = labels_part[~idx_iou]
-        # print("rail, bkg:",pred_labels.shape,pred_labels_rev.shape)
-        # print(pred_labels[pred_labels == 1].shape, pred_labels_rev[pred_labels_rev == 0].shape)
-        # exit()
-        iou_res = len(pred_labels[pred_labels == 1]) / (len(pred_labels[pred_labels == 0]) +
-                                                        len(pred_labels_rev[pred_labels_rev == 1]) + len(
-                    pred_labels[pred_labels == 1]))
-        print("iou: ", iou_res)
-        return iou_res
+            points_part_sub = points_part[idx_part_sub]
+            #surface segmentation
+            pcd_inner.points = o3d.utility.Vector3dVector(points_part_sub)
+            plane_model, inliers = pcd_inner.segment_plane(distance_threshold=0.3,
+                                                          ransac_n=7,
+                                                          num_iterations=100)
+            inner_idx = np.zeros(len(points_part_sub)).astype(np.bool)
+            inner_idx[inliers] = True
+            surface_sub = points_part_sub[inner_idx]
+            noise_sub = points_part_sub[~inner_idx]
+
+            surface_points.append(surface_sub)
+            noise_points.append(noise_sub)
+            # print("test:", j, ":",surface_sub.shape,noise_sub.shape)
+    surface_arr = np.concatenate(surface_points,axis=0)
+    noise_arr = np.concatenate(noise_points,axis=0)
+    print("surface points:", surface_arr.shape,"  noise points:", noise_arr.shape)
+
+    surface_show = o3d.geometry.PointCloud()
+    surface_show.points = o3d.utility.Vector3dVector(surface_arr)
+    surface_color = np.ones(surface_arr.shape[0], dtype=np.int)
+    surface_show.colors = o3d.utility.Vector3dVector(label2color(surface_color))
+
+    noise_show = o3d.geometry.PointCloud()
+    noise_show.points = o3d.utility.Vector3dVector(noise_arr)
+    noise_color = np.ones(noise_arr.shape[0], dtype=np.int) * 3
+    noise_show.colors = o3d.utility.Vector3dVector(label2color(noise_color))
+    mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    mesh.scale(2, center=mesh.get_center())
+    o3d.visualization.draw_geometries([surface_show,noise_show,mesh])
+
+    # exit()
+
 
 
 if __name__ == '__main__':
-    print(1)
+
     #实现功能：论文第4章实验部分。
-    #1.轨道线拟合，求IoU
+
+    # 1.轨道线拟合，求IoU
     seq_root = "D:/code/dataset/pc_npy_a9"
     seq_names =  os.listdir(seq_root)
 
     iou_res_all = []
     for seq_name in seq_names:
-        if seq_name == "a1" or seq_name == "a2" or seq_name == "a6" or seq_name == "a7": #
+        # if seq_name == "a1" or seq_name == "a2" or seq_name == "a6" or seq_name == "a7": #
         # if seq_name == "a5a" or seq_name == "a5b" or seq_name == "a5c" or seq_name == "errorlabel": #
-        # if seq_name == "a3a" or seq_name == "a3b": #
+        if seq_name == "a5c" or seq_name == "a3b": #
 
             # file_root = "D:/code/dataset/pc_npy_a9/a1"
             file_root = os.path.join(seq_root,seq_name)
             files = os.listdir(file_root)
 
             for i, file_name in enumerate(files):
-                npy_path = os.path.join(file_root,file_name)
-                raw_arr = np.load(npy_path)
-                points_arr = raw_arr[:, (2, 1, 0)]
-                labels_arr = raw_arr[:, -1].astype(np.int)
-                iou_res = poly_fit_iou_ours(points_arr, labels_arr)
-                if iou_res != 'error1':
-                    iou_res_all.append(iou_res)
+                if i >100:
+                    npy_path = os.path.join(file_root,file_name)
+                    raw_arr = np.load(npy_path)
+                    points_arr = raw_arr[:, (2, 1, 0)]
+                    labels_arr = raw_arr[:, -1].astype(np.int)
+                    iou_res = poly_fit_iou_ours(points_arr, labels_arr)
+                    if iou_res != 'error1':
+                        iou_res_all.append(iou_res)
 
     print(len(iou_res_all), np.mean(iou_res_all))
     exit()
+    print(1)
     #2.安全行车区域
     #3.路面分割
     file_path = "../datareal/npyfile/a1/1587955555044845.npy"
@@ -233,27 +292,9 @@ if __name__ == '__main__':
 
     # step 1. 提取出轨道的点云
     rail_array = points_array[labels_array == 1]
-    # step 2. 步进的距离，
-    step_x = 5.0 # 步进 x 米
-    step_bais = 6.0 # 从 bais 米开始
-    rail_part_d = [4,9,13,17,21]
-    rail_cross = 2
-    rail_part_range = [6,10,19,32,49,70]
-    print("max and min of rail : ",np.max(rail_array,axis=0))
-    print("min and min of rail : ",np.min(rail_array,axis=0))
 
     pcd_box = []
-    # for i, dist in enumerate(rail_part_range):
-    #     if i <= 4:
-    #         part_idx = (rail_array[:, 0] >= rail_part_range[i]-rail_cross) & (rail_array[:, 0] < rail_part_range[i + 1])
-    #         part_rail_points = rail_array[part_idx]
-    #         part_box = poly_fit_x(part_rail_points)
-    #         pcd_box.append(part_box)
 
-    # pcd_box = []
-    # for i in range(0,10):
-    #     part_idx = (rail_array[:,0] >= (step_bais + i*step_x)) & (rail_array[:,0] <= (step_bais + (i+1)*step_x))
-    #     part_rail_points = rail_array[part_idx]
     part_box = poly_fit_x(rail_array)
     pcd_box.append(part_box)
 
